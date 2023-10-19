7b2314121b52d5b94659e16c1947fadd99ec21e9
rakesh <thotarakesh28@gmail.com>
2023-10-19 03:36:46 +0000

    solution

---
+++ src/Components/NotFound/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+
+const NotFound = () => (
+  <>
+    <Header />
+    <div>
+      <div>
+        <img src="" alt="not found" />
+        <h1>Page Not Found</h1>
+        <p>We are sorry, the page you requested could not be found</p>
+      </div>
+    </div>
+  </>
+)
+
+export default NotFound
